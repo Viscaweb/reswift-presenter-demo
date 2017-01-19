a1750436c8e35a3c2abdd8bd883f7fbe7af94cbc
lluisgh28 <lluisgh28@gmail.com>
2017-01-19 10:10:02 +0100

    Project setup with Carthage

---
+++ ReSwift Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReSwift Example
+//
+//  Created by Lluís Gómez on 19/01/2017.
+//  Copyright © 2017 Top Affiliate Publishing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
