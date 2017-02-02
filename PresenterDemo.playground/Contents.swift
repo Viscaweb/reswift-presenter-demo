39b1293faec27d860ff9ff04795ded8e0d341c05
Damien Gavard <damien@viscaweb.com>
2017-02-02 22:26:35 +0100

    Add full demo including subscription on viewWillAppear, the router, etc

--- PresenterDemo.playground/Contents.swift
+++ PresenterDemo.playground/Contents.swift
@@ -3,7 +3,49 @@
 import UIKit
 import PlaygroundSupport
 import ReSwift
+import ReSwiftRouter
 
+
+
+
+
+/*
+ ██████╗ ██╗      █████╗ ██╗   ██╗ ██████╗ ██████╗  ██████╗ ██╗   ██╗███╗   ██╗██████╗
+ ██╔══██╗██║     ██╔══██╗╚██╗ ██╔╝██╔════╝ ██╔══██╗██╔═══██╗██║   ██║████╗  ██║██╔══██╗
+ ██████╔╝██║     ███████║ ╚████╔╝ ██║  ███╗██████╔╝██║   ██║██║   ██║██╔██╗ ██║██║  ██║
+ ██╔═══╝ ██║     ██╔══██║  ╚██╔╝  ██║   ██║██╔══██╗██║   ██║██║   ██║██║╚██╗██║██║  ██║
+ ██║     ███████╗██║  ██║   ██║   ╚██████╔╝██║  ██║╚██████╔╝╚██████╔╝██║ ╚████║██████╔╝
+ ╚═╝     ╚══════╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═══╝╚═════╝
+ */
+
+PlaygroundPage.current.needsIndefiniteExecution = true // Because Router dispatch async on Main queue
+
+class Logger {
+    private static let startTime = Date()
+
+    static func log(_ text: String) {
+        let currentTime = Date()
+        let elapsedTime = currentTime.timeIntervalSince(startTime)
+        let elapsedMS = String(format: "%.2f", abs(elapsedTime))
+
+        print("+\(elapsedMS) - \(text)")
+    }
+}
+
+
+
+
+
+
+/*
+ ███████╗██████╗  █████╗ ███╗   ███╗███████╗██╗    ██╗ ██████╗ ██████╗ ██╗  ██╗
+ ██╔════╝██╔══██╗██╔══██╗████╗ ████║██╔════╝██║    ██║██╔═══██╗██╔══██╗██║ ██╔╝
+ █████╗  ██████╔╝███████║██╔████╔██║█████╗  ██║ █╗ ██║██║   ██║██████╔╝█████╔╝
+ ██╔══╝  ██╔══██╗██╔══██║██║╚██╔╝██║██╔══╝  ██║███╗██║██║   ██║██╔══██╗██╔═██╗
+ ██║     ██║  ██║██║  ██║██║ ╚═╝ ██║███████╗╚███╔███╔╝╚██████╔╝██║  ██║██║  ██╗
+ ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝ ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝
+
+*/
 
 // ----------------------------------------------------------------------
 // VIEW
@@ -25,7 +67,7 @@ protocol ConfigurableView: ViewType {
 extension ConfigurableView where Self: UIViewController {
     
     func update(with viewModel: ViewModel) {
-        print("> ViewController: updateVM")
+        Logger.log("ViewController: update(with:)")
         
         self.viewModel = viewModel
         
@@ -36,7 +78,6 @@ extension ConfigurableView where Self: UIViewController {
         configureView()
     }
 }
-
 
 
 // ----------------------------------------------------------------------
@@ -62,7 +103,7 @@ class Presenter<Adapter: ViewAdapter>: StoreSubscriber  {
     }
     
     final func newState(state: State) {
-        print("> Presenter: newState")
+        Logger.log("Presenter: newState : \(state)")
         let viewModel = mapper.viewModel(for: state)
         view.update(with: viewModel)
     }
@@ -88,112 +129,267 @@ class Interactor<Adapter: ViewAdapter> {
     }
     
     func subscribe() {
+        Logger.log("Interactor: subscribe")
         store.subscribe(presenter, selector: stateSelector)
     }
     
     func unsubscribe() {
+        Logger.log("Interactor: unsubscribe")
         store.unsubscribe(presenter)
     }
     
     func dispatch(_ action: Action) {
+        Logger.log("Interactor: dispatch(action)")
         store.dispatch(action)
     }
     
     func dispatch(_ actionCreator: @escaping Store<AppState>.ActionCreator) {
+        Logger.log("Interactor: dispatch(actionCreator)")
         store.dispatch(actionCreator)
     }
 }
 
 
-// ----------------------------------------------------------------------
-// IMPLEMENTATIONS: MatchCalendar Module
-// ----------------------------------------------------------------------
 
-struct MatchCalendarState: StateType {
+
+
+
+
+
+/*
+ ██╗  ██╗ ██████╗ ███╗   ███╗███████╗    ███╗   ███╗ ██████╗ ██████╗ ██╗   ██╗██╗     ███████╗
+ ██║  ██║██╔═══██╗████╗ ████║██╔════╝    ████╗ ████║██╔═══██╗██╔══██╗██║   ██║██║     ██╔════╝
+ ███████║██║   ██║██╔████╔██║█████╗      ██╔████╔██║██║   ██║██║  ██║██║   ██║██║     █████╗
+ ██╔══██║██║   ██║██║╚██╔╝██║██╔══╝      ██║╚██╔╝██║██║   ██║██║  ██║██║   ██║██║     ██╔══╝
+ ██║  ██║╚██████╔╝██║ ╚═╝ ██║███████╗    ██║ ╚═╝ ██║╚██████╔╝██████╔╝╚██████╔╝███████╗███████╗
+ ╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝    ╚═╝     ╚═╝ ╚═════╝ ╚═════╝  ╚═════╝ ╚══════╝╚══════╝
+ */
+
+typealias HomeInteractor = Interactor<HomeAdapter>
+
+struct HomeState: StateType {
     let title: String
 }
 
-struct MatchCalendarViewModel {
+struct SetTitleAction: Action {
     let title: String
 }
 
-struct MatchCalendarAdapter: ViewAdapter {
-    typealias State = MatchCalendarState
-    typealias View = MatchCalendarViewController
+struct HomeViewModel {
+    let title: String
+}
+
+struct HomeAdapter: ViewAdapter {
+    typealias State = HomeState
+    typealias View = HomeViewController
     
-    func viewModel(for state: MatchCalendarState) -> MatchCalendarViewModel {
-        return MatchCalendarViewModel(title: state.title)
+    func viewModel(for state: HomeState) -> HomeViewModel {
+        return HomeViewModel(title: state.title)
     }
 }
 
-class MatchCalendarViewController: UIViewController, ConfigurableView {
-    typealias ViewModel = MatchCalendarViewModel
+class HomeViewController: UIViewController, ConfigurableView {
+    typealias ViewModel = HomeViewModel
 
-    internal var viewModel: MatchCalendarViewModel?
-    var interactor: MatchCalendarInteractor!
+    internal var viewModel: HomeViewModel?
+    var interactor: HomeInteractor!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("> ViewController: viewDidload")
+        Logger.log("ViewController: viewDidload")
 
         view.frame = CGRect(
             x: 0, y: 0, width: 320, height: 480)
         view.backgroundColor = .white
-        
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         interactor.subscribe()
     }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        interactor.unsubscribe()
+    }
+
     func configureView() {
-        let title = viewModel?.title ?? "nil vm"
-        print("> ViewController: configureView \(title)\n")
+        Logger.log("ViewController: configureView, viewModel = \(viewModel)")
     }
 }
 
-typealias MatchCalendarInteractor = Interactor<MatchCalendarAdapter>
+class HomeFactory {
 
-class MatchCalendarFactory {
+    static func createViewController() -> HomeViewController {
+        guard let store = Container.store else {
+            fatalError("Not store available")
+        }
 
-    static func createViewController(with store: Store<AppState>) -> UIViewController {
-        let mapper = MatchCalendarAdapter()
-        let viewController = MatchCalendarViewController()
+        let mapper = HomeAdapter()
+        let viewController = HomeViewController()
         let presenter = Presenter(mapper: mapper, view: viewController)
-        let interactor = Interactor(presenter: presenter, store: store) { $0.calendar }
+        let interactor = Interactor(presenter: presenter, store: store) { $0.home }
         viewController.interactor = interactor
+
         return viewController
     }
 }
 
 
+
+
+
+
+
+
+/*
+ ██████╗ ███████╗███╗   ███╗ ██████╗
+ ██╔══██╗██╔════╝████╗ ████║██╔═══██╗
+ ██║  ██║█████╗  ██╔████╔██║██║   ██║
+ ██║  ██║██╔══╝  ██║╚██╔╝██║██║   ██║
+ ██████╔╝███████╗██║ ╚═╝ ██║╚██████╔╝
+ ╚═════╝ ╚══════╝╚═╝     ╚═╝ ╚═════╝
+ */
+
+
 // ----------------------------------------------------------------------
-// DEMO
+// MODULE FACTORY
 // ----------------------------------------------------------------------
 
-// ReSwift stuff -----------------------
-struct SetTitleAction: Action {
-    let title: String
+class ViewControllersFactory {
+    static func create(for name: String) -> UIViewController {
+        switch name {
+        case "home":
+            return HomeFactory.createViewController()
+        default:
+            fatalError("Unsported \(name) controller")
+        }
+    }
+}
+
+
+// ----------------------------------------------------------------------
+// ROUTING
+// ----------------------------------------------------------------------
+
+extension UIViewController: Routable {
+    public func pushRouteSegment(_ routeElementIdentifier: RouteElementIdentifier,
+                                 animated: Bool, completionHandler: @escaping RoutingCompletionHandler) -> Routable {
+
+        Logger.log("ViewController: pushRouteSegment: Push \"\(routeElementIdentifier)\" from \(self)")
+
+        let nextViewController = ViewControllersFactory.create(for: routeElementIdentifier)
+
+        if let selfAsNavVC = self as? UINavigationController {
+            selfAsNavVC.pushViewController(nextViewController, animated: animated)
+        } else {
+            self.navigationController!.pushViewController(nextViewController, animated: animated)
+        }
+
+        completionHandler()
+
+        return nextViewController
+    }
+
+    public func popRouteSegment(_ routeElementIdentifier: RouteElementIdentifier, animated: Bool, completionHandler: @escaping RoutingCompletionHandler) {
+
+        Logger.log("ViewController: popRouteSegment")
+
+        if let selfAsNavVC = self as? UINavigationController {
+            selfAsNavVC.popViewController(animated: animated)
+        } else {
+            self.navigationController!.popViewController(animated: animated)
+        }
+
+        completionHandler()
+    }
+
+    public func changeRouteSegment(_ from: RouteElementIdentifier, to: RouteElementIdentifier, animated: Bool, completionHandler: @escaping RoutingCompletionHandler) -> Routable {
+
+        Logger.log("ViewController: changeRouteSegment")
+
+        let nextViewController = ViewControllersFactory.create(for: to)
+
+        if let selfAsNavVC = self as? UINavigationController {
+            selfAsNavVC.viewControllers.removeLast()
+            selfAsNavVC.viewControllers.append(nextViewController)
+        } else {
+            self.navigationController!.viewControllers.removeLast()
+            self.navigationController!.viewControllers.append(nextViewController)
+        }
+        
+        return nextViewController
+        
+    }
+}
+
+
+
+// ----------------------------------------------------------------------
+// APPLICATION
+// ----------------------------------------------------------------------
+
+/// Just to share an instance
+class Container {
+    static var store: Store<AppState>?
 }
 
 struct AppState: StateType {
-    let calendar: MatchCalendarState
+    let navigationState: NavigationState
+    let home: HomeState
 }
 
 func appReducer(action: Action, state: AppState?) -> AppState {
+
+    Logger.log("Reducer: state = \(state)")
+    Logger.log("Reducer: action = \(action)")
+
     var title = "DEFAULT"
     if let titleAction = action as? SetTitleAction {
         title = titleAction.title
     }
-    
-    print("> Reducer: \(title)")
-    return AppState(calendar: MatchCalendarState(title: title))
+
+    let navState = NavigationReducer.handleAction(action, state: state?.navigationState)
+    Logger.log("Reducer: navState = \(navState)")
+
+    let newState = AppState(
+        navigationState: navState,
+        home: HomeState(title: title)
+    )
+
+    Logger.log("Reducer: newState = \(newState)")
+
+    return newState
 }
 
 
-// Instantiation -----------------------
-let store = Store(reducer: appReducer, state: nil, middleware: [])
-let viewController = MatchCalendarFactory.createViewController(with: store)
+// ----------------------------------------------------------------------
+// SETUP AND RUN
+// ----------------------------------------------------------------------
 
-// Execution -----------------------
-PlaygroundPage.current.liveView = viewController
-sleep(1)
-store.dispatch(SetTitleAction(title: "NEW"))
-PlaygroundPage.current.finishExecution()
+let defaultRoute = ["home"]
+let initialState = AppState(
+    navigationState: NavigationState(),
+    home: HomeState(title: "DEFAULT")
+)
+let store = Store(reducer: appReducer, state: initialState)
+Container.store = store
+
+let rootViewController = UINavigationController()
+let router = Router(store: store, rootRoutable: rootViewController) { $0.navigationState}
+
+PlaygroundPage.current.liveView = rootViewController
+
+_ = store.dispatch { state, _ in
+    if state.navigationState.route.isEmpty {
+        return SetRouteAction(defaultRoute)
+    } else {
+        return nil
+    }
+}
+
+// Dispatch another routing action 2s later
+DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(2000)) {
+    store.dispatch(SetRouteAction(["home", "home"], animated: true))
+    Logger.log("DONE")
+}
