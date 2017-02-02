aea3b50e73f9509bc1169678e722e35f1a8b220b
lluisgh28 <lluisgh28@gmail.com>
2017-02-02 09:46:48 +0100

    Nicer Presenter without injected function hack

--- PresenterDemo.playground/Contents.swift
+++ PresenterDemo.playground/Contents.swift
@@ -9,57 +9,59 @@ import ReSwift
 // PRESENTER
 // ----------------------------------------------------------------------
 
-protocol Mapper {
+protocol MapperType {
     associatedtype State
     associatedtype ViewModel
     func viewModel(for state: State) -> ViewModel
 }
 
 
-class Presenter<M: Mapper>: StoreSubscriber {
-    
+class Presenter<M: MapperType, V: ViewType>: StoreSubscriber where M.ViewModel == V.ViewModel {
     typealias StoreSubscriberStateType = M.State
     typealias State = M.State
     typealias ViewModel = M.ViewModel
     
-    final internal var updateView: ((ViewModel) -> ())?
-    
     private let mapper: M
-    init(mapper: M) {
+    private let view: V
+    
+    init(mapper: M, view: V) {
         self.mapper = mapper
+        self.view = view
     }
+    
     final func newState(state: State) {
-        updateView!(mapper.viewModel(for: state))
+        let viewModel = mapper.viewModel(for: state)
+        view.update(with: viewModel)
     }
 }
 
 // ----------------------------------------------------------------------
-// VIEW CONTROLLER
+// VIEW
 // ----------------------------------------------------------------------
 
 protocol ViewType: class {
-    associatedtype ViewModelType
-
-    func update(with viewModel: ViewModelType)
+    associatedtype ViewModel
+    
+    func update(with viewModel: ViewModel)
 }
 
 protocol ConfigurableView: ViewType {
-    var viewModel: ViewModelType? {get set}
-
+    var viewModel: ViewModel? {get set}
+    
     func configureView()
 }
 
 extension ConfigurableView where Self: UIViewController {
-
-    func update(with viewModel: ViewModelType) {
+    
+    func update(with viewModel: ViewModel) {
         print("> ViewController: updateVM")
-
+        
         self.viewModel = viewModel
-
+        
         guard isViewLoaded else {
             return
         }
-
+        
         configureView()
     }
 }
@@ -85,7 +87,7 @@ struct MatchCalendarViewModel {
     let title: String
 }
 
-struct MatchCalendarMapper: Mapper {
+struct MatchCalendarMapper: MapperType {
     typealias State = MatchCalendarState
     typealias ViewModel = MatchCalendarViewModel
     
@@ -96,7 +98,7 @@ struct MatchCalendarMapper: Mapper {
 
 class MatchCalendarViewController: UIViewController, ConfigurableView {
 
-    typealias ViewModelType = MatchCalendarViewModel
+    typealias ViewModel = MatchCalendarViewModel
 
     internal var viewModel: MatchCalendarViewModel?
 
@@ -120,14 +122,15 @@ class MatchCalendarViewController: UIViewController, ConfigurableView {
 class MatchCalendarFactory {
 
     typealias MatchCalendarModule = (
-        presenter: Presenter<MatchCalendarMapper>,
+        presenter: Presenter<MatchCalendarMapper, MatchCalendarViewController>,
         viewController: MatchCalendarViewController
     )
 
     static func create(with store: Store<AppState>) -> MatchCalendarModule {
-        let presenter = Presenter<MatchCalendarMapper>(mapper: MatchCalendarMapper())
+        let mapper = MatchCalendarMapper()
         let viewController = MatchCalendarViewController()
-        presenter.updateView = viewController.update(with:)
+        let presenter = Presenter(mapper: mapper, view: viewController)
+        
         store.subscribe(presenter) { $0.calendar }
         
         return (presenter, viewController)
